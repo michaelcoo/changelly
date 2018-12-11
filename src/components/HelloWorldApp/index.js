b901356a8c2a09f219d3757ab241201e7f43f0fc
Yang Tao <yangtao@tik.com>
2018-12-11 14:28:39 +0800

    [feature] first commit
    Signed-off-by: Yang Tao <yangtao@tik.com>

---
+++ src/components/HelloWorldApp/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Text, Alert, AppRegistry, Button, StyleSheet, View } from 'react-native';
+
+class HelloWorldApp extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Hello world!</Text>
+
+      </View>
+    );
+  }
+}
+
+export default HelloWorldApp;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { Text, Alert, AppRegistry, Button, StyleSheet, View } from 'react-native';
+import  HelloWorldApp from './components/HelloWorldApp'
+
+export default class App extends Component {
+  render() {
+    return (
+      <HelloWorldApp/>
+    );
+  }
+}
